f076909215ad96757edb347b90f3a89ad3e6e614
許　志偉 <01027698@CA4857.local>
2019-05-24 13:44:37 +0900

    add test

---
+++ src/test/java/com/kyoshii/chapter2/CustomerServiceTest.java
@@ -0,0 +1,20 @@
+package com.kyoshii.chapter2;
+
+import com.kyoshii.chapter2.service.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomerServiceTest {
+    private final CustomerService customerService;
+
+    public CustomerServiceTest() {
+        this.customerService = new CustomerService();
+    }
+
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+}
