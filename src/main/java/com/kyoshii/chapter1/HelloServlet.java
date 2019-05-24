14667b55f119a81c96c57afa23fa325074fc0ace
許　志偉 <01027698@CA4857.local>
2019-05-24 11:12:16 +0900

    init web-framework

---
+++ src/main/java/com/kyoshii/chapter1/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.kyoshii.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet(name = "HelloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        request.setAttribute("currentTime",currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+    }
+}
