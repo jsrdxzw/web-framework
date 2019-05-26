e346af5dc1464f027bad46f6aec494256d6d1ce2
xuzhiwei <jsrdxzw@163.com>
2019-05-26 12:22:47 +0900

    add reflection class

---
+++ src/main/java/com/kyoshii/util/ReflectionUtil.java
@@ -0,0 +1,68 @@
+package com.kyoshii.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @Author: xuzhiwei
+ * @Date: 2019-05-26
+ * @Description: 反射工具类
+ */
+public class ReflectionUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     *
+     * @param cls
+     * @return
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance = null;
+        try {
+            instance = cls.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            LOGGER.error("new instance failure", e);
+        }
+        return instance;
+    }
+
+    /**
+     * 通过反射调用方法
+     *
+     * @param obj
+     * @param method
+     * @param args
+     * @return
+     */
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result = null;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            LOGGER.error("method invoke failure", e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     *
+     * @param obj
+     * @param field
+     * @param value
+     */
+    public static void setField(Object obj, Field field, Object value) {
+        field.setAccessible(true);
+        try {
+            field.set(obj, value);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("set field failure", e);
+        }
+    }
+}
